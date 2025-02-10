7193fa00368ee13b58e70cdb6a86ba6d1847e978
Nolann Dubos <nolann.dubos@epitech.eu>
2025-02-10 12:42:53 +0100

    remove components.hpp

--- include/Acomponent.hpp
+++ include/Acomponent.hpp
@@ -1,15 +1,15 @@
 #ifndef ACOMPONENTS_HPP_
-    #define ACOMPONENTS_HPP_
+#define ACOMPONENTS_HPP_
 
-    #include <iostream>
-    #include "components.hpp"
+#include "nts.hpp"
 
-    class AComponent : public nts::IComponent {
-        public:
-            virtual ~AComponent() = default;
-            virtual void simulate (std::size_t tick);
-            virtual nts::Tristate compute(std::size_t pin) = 0;
-            virtual void setLink (std::size_t pin, nts::IComponent &other, std::size_t otherPin);
-    };
+class AComponent : public nts::IComponent {
+public:
+  virtual ~AComponent() = default;
+  virtual void simulate(std::size_t tick);
+  virtual nts::Tristate compute(std::size_t pin) = 0;
+  virtual void setLink(std::size_t pin, nts::IComponent &other,
+                       std::size_t otherPin);
+};
 
 #endif
