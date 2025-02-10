85a28e6a155249c9a1e3e851117829ac7077adfa
etienne <etienne.labarbe@epitech.eu>
2025-02-10 11:25:03 +0100

    add Acomponent.hpp / rename components.hpp -> nts.hpp

---
+++ include/nts.hpp
@@ -0,0 +1,23 @@
+#ifndef COMPONENTS_HPP_
+    #define COMPONENTS_HPP_
+
+    #include <iostream>
+
+    namespace nts
+    {
+        enum Tristate {
+            Undefined = (-true),
+            True = true,
+            False = false
+        };
+
+        class IComponent {
+            public:
+                virtual ~IComponent() = default;
+                virtual void simulate (std::size_t tick) = 0;
+                virtual nts::Tristate compute(std::size_t pin) = 0;
+                virtual void setLink (std::size_t pin, nts::IComponent &other, std::size_t otherPin) = 0;
+        };
+    }
+
+#endif
