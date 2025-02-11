119e4da7da918c2aa871bfc680f0f4b1e0f17d0b
Nolann Dubos <nolann.dubos@epitech.eu>
2025-02-11 18:42:29 +0100

    fix Acomponent destructor

--- include/Acomponent.hpp
+++ include/Acomponent.hpp
@@ -8,9 +8,9 @@
 namespace nts {
 class AComponent : public nts::IComponent {
 public:
-  virtual ~AComponent();
-  virtual void simulate(std::size_t tick) override;
-  virtual nts::Tristate compute(std::size_t pin) override;
+  virtual ~AComponent() = default;
+  virtual void simulate(std::size_t tick) override {};
+  virtual nts::Tristate compute(std::size_t pin) override {};
   virtual void setLink(std::size_t pin, nts::IComponent &other,
                        std::size_t otherPin) override;
 
--- src/AComponent.cpp
+++ src/AComponent.cpp
@@ -2,8 +2,6 @@
 #include "nts.hpp"
 #include <utility>
 
-nts::AComponent::~AComponent() = default;
-
 void nts::AComponent::setLink(std::size_t pin, nts::IComponent &other,
                               std::size_t otherPin) {
   _connection[pin] = std::make_pair(&other, otherPin);
