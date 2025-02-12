88b5df2a29d069528b4627d81d23fa67fe5e35c6
Nolann Dubos <nolann.dubos@epitech.eu>
2025-02-12 12:23:09 +0100

    add AndGate

---
+++ include/AndGate.hpp
@@ -0,0 +1,19 @@
+#ifndef AND_HPP_
+#define AND_HPP_
+
+#include "Acomponent.hpp"
+
+namespace nts {
+
+class AndGate : public nts::AComponent {
+public:
+  AndGate() = default;
+
+  nts::Tristate compute(std::size_t pin) override;
+
+private:
+  std::vector<std::size_t> _pins = {0, 0, 0};
+};
+
+} // namespace nts
+#endif
