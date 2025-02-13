118dfa918865ef50bad5a9cae72607f2756d8104
etienne <etienne.labarbe@epitech.eu>
2025-02-13 18:23:48 +0100

    add clock

--- include/clockComponent.hpp
+++ include/clockComponent.hpp
@@ -2,12 +2,21 @@
 #define CLOCKCOMPONENTS_HPP_
 
 #include "SpecialComponent.hpp"
+#include "nts.hpp"
 
 namespace nts {
 class ClockComponent : public nts::SpecialComponent {
 public:
   ~ClockComponent() = default;
-  ClockComponent() = default;
+  ClockComponent() {setState(nts::Tristate::Undefined);};
+
+   void simulate(std::size_t tick) override {
+    (void)tick;
+    if (_state == nts::Tristate::True)
+      _state = nts::Tristate::False;
+    else
+      _state = nts::Tristate::False;
+  };
 };
 } // namespace nts
 
